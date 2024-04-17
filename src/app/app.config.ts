e5d07e8efee4b66d9f2192989daf868c423c6381
TOME Olivier <olivier.tome@sinequa.com>
2024-04-17 13:57:18 +0200

    ✨ ES-22284 - (preview) add preview's actions buttons

--- src/app/app.config.ts
+++ src/app/app.config.ts
@@ -4,7 +4,7 @@ import localeFr from '@angular/common/locales/fr';
 import { APP_INITIALIZER, ApplicationConfig, LOCALE_ID } from '@angular/core';
 import { provideRouter, withComponentInputBinding, withHashLocation } from '@angular/router';
 
-import { appInitializerFn, auditInterceptorFn, authInterceptorFn, bodyInterceptorFn, errorInterceptorFn } from '@sinequa/atomic-angular';
+import { HIGHLIGHTS, appInitializerFn, auditInterceptorFn, authInterceptorFn, bodyInterceptorFn, errorInterceptorFn } from '@sinequa/atomic-angular';
 
 import { routes } from '@/app/app.routes';
 import { SearchInputService } from './components/search-input/search-input.service';
@@ -14,11 +14,41 @@ import { queryNameInterceptorFn } from './utils/queryname.interceptor';
 
 registerLocaleData(localeFr);
 
+
+const PREVIEW_HIGHLIGHTS = [
+  {
+      name: 'company',
+      color: 'white',
+      bgColor: '#FF7675'
+  },
+  {
+      name: 'geo',
+      color: 'white',
+      bgColor: '#74B9FF'
+  },
+  {
+      name: 'person',
+      color: 'white',
+      bgColor: '#00ABB5'
+  },
+  {
+      name: 'extractslocations',
+      color: 'black',
+      bgColor: '#fffacd'
+  },
+  {
+      name: 'matchlocations',
+      color: 'black',
+      bgColor: '#ff0'
+  }
+];
+
 export const appConfig: ApplicationConfig = {
   providers: [
     // set the default OAuth2 and/or SAML authentication provider
     { provide: APP_INITIALIZER, useFactory: () => appInitializerFn, multi: true },
     { provide: LOCALE_ID, useValue: 'fr-FR' },
+    { provide: HIGHLIGHTS, useValue: PREVIEW_HIGHLIGHTS},
     provideRouter(routes, withHashLocation(), withComponentInputBinding()),
     eagerProvider(QueryParamsService),
     eagerProvider(SearchInputService),
