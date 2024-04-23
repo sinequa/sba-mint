6f9b1b5124653a38e191f43e0b3d4c0f4cfde527
TOME Olivier <olivier.tome@sinequa.com>
2024-04-23 12:59:47 +0200

    💄 ES-21784 - (tailwindcss) fix some css glitches after reworking

--- src/app/wps-components/author-avatar/author-avatar.component.ts
+++ src/app/wps-components/author-avatar/author-avatar.component.ts
@@ -1,6 +1,6 @@
 import { PersonArticle } from '@/app/types';
 import { NgClass } from '@angular/common';
-import { Component, computed, effect, input, signal } from '@angular/core';
+import { Component, computed, input, signal } from '@angular/core';
 import { AvatarInitialsComponent } from "./avatar-initials";
 
 
@@ -26,15 +26,12 @@ import { AvatarInitialsComponent } from "./avatar-initials";
     standalone: true,
     templateUrl: './author-avatar.component.html',
     // eslint-disable-next-line @angular-eslint/no-host-metadata-property
-    host: { class: 'avatar' },
     imports: [NgClass, AvatarInitialsComponent]
 })
 export class AuthorAvatarComponent {
   person = input.required<Partial<PersonArticle> | undefined>();
   className = input<string>();
-  useImage = input<boolean>(true);
 
-  withImage = signal(this.useImage());
   imageUrl = computed(() => this.person()?.employeePhotoURL );
   initials = computed(() => {
 
@@ -43,7 +40,7 @@ export class AuthorAvatarComponent {
     if(employeeFullName) {
       const initials = employeeFullName
         .split(' ')
-        .filter( word => word[0] === word[0].toUpperCase() )
+        .filter( word => word[0] && (word[0] === word[0].toUpperCase()) )
         .map( word => word[0] )
         .join('');
 
@@ -53,12 +50,4 @@ export class AuthorAvatarComponent {
   })
 
   protected readonly imgLoadFailed = signal(false);
-
-  constructor() {
-    effect(() => {
-      if(this.initials().length === 0) {
-        this.withImage.set(true);
-      }
-    }, {allowSignalWrites: true})
-  }
 }
