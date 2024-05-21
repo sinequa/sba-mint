fff4df55f782c2269174053757be029edbf39ceb
Loïc Juillet <loic.juillet@sinequa.com>
2024-05-21 10:28:54 +0200

    ✨ assistant in assistant drawer

--- src/app/components/drawer/components/assistant/assistant.component.ts
+++ src/app/components/drawer/components/assistant/assistant.component.ts
@@ -1,14 +1,22 @@
 import { NgClass } from '@angular/common';
-import { Component } from '@angular/core';
+import { Component, computed, inject } from '@angular/core';
+
+import { AssistantComponent } from '@/app/components/assistant/assistant';
+import { ApplicationStore } from '@/stores';
+
 import { DrawerComponent } from '../../drawer.component';
 import { DrawerService } from '../../drawer.service';
 
 @Component({
   selector: 'app-drawer-assistant',
   standalone: true,
-  imports: [NgClass],
+  imports: [NgClass, AssistantComponent],
   providers: [DrawerService],
   templateUrl: './assistant.component.html',
   styleUrls: ['../../drawer.component.scss', './assistant.component.scss']
 })
-export class DrawerAssistantComponent extends DrawerComponent { }
+export class DrawerAssistantComponent extends DrawerComponent {
+  protected isAssistantReady = computed(() => this.applicationStore.assistantReady());
+
+  private applicationStore = inject(ApplicationStore);
+}
