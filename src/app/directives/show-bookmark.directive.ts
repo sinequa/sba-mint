d5859be07891e04f26316b1f8c2e8ea2b3b6a43c
Chevillard <tanguy.chevillard@sinequa.com>
2024-05-23 15:12:15 +0200

    feat: 🪄 created ShowBookmarkDirective

---
+++ src/app/directives/show-bookmark.directive.ts
@@ -0,0 +1,38 @@
+import { Directive, HostListener, computed, effect, inject, input, output, signal } from '@angular/core';
+import { Article } from '@sinequa/atomic';
+import { UserSettingsStore } from '../stores';
+
+@Directive({
+  selector: '[appShowBookmark]',
+  standalone: true
+})
+export class ShowBookmarkDirective {
+  @HostListener('mouseenter')
+  public mouseEnter(): void {
+    this.bookmarkHovered.set(true);
+  }
+
+  @HostListener('mouseleave')
+  public mouseLeave(): void {
+    this.bookmarkHovered.set(false);
+  }
+
+  protected bookmarkHovered = signal(false);
+  userSettingsStore = inject(UserSettingsStore);
+
+  protected isBookmarked = computed(() => {
+    return this.article() && this.article()!.id ? this.userSettingsStore.isBookmarked(this.article()!.id!) : false;
+  });
+
+  public readonly article = input.required<Article | Partial<Article> | undefined>();
+  public readonly showBookmark = output<boolean>();
+
+  constructor() {
+    effect(() => {
+      const bookmarkHovered = this.bookmarkHovered();
+      const isBookmarked = this.isBookmarked();
+      this.showBookmark.emit(bookmarkHovered || isBookmarked);
+    });
+  }
+
+}
