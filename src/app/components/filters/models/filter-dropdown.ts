a911b17a27f2f9e2c7913b351a2b0b63e90e872f
TOME Olivier <olivier.tome@sinequa.com>
2024-06-04 09:52:20 +0200

    ✨ ES-22662 (filters) Allow to display custom name for aggregations (filters)

--- src/app/components/filters/models/filter-dropdown.ts
+++ src/app/components/filters/models/filter-dropdown.ts
@@ -3,10 +3,10 @@ import { Filter } from "@/app/utils";
 
 
 export type FilterDropdown = {
-  label: string;
-  aggregation: AggregationEx;
-  icon?: string;
-  currentFilter?: Filter;
-  value?: { operator?: string, text: string, display?: string };
-  moreFiltersCount?: number;
+  label: string;              // The label of the filter
+  aggregation: AggregationEx; // The aggregation object
+  icon?: string;              // The icon of the filter
+  currentFilter?: Filter;     // The current filters
+  value?: { operator?: string, text: string, display?: string }; // The value of the filter first filter
+  moreFiltersCount?: number;  // The number of filters that are not displayed
 }
