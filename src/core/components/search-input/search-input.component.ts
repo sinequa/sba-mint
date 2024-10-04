74d93d5cefb92053499ade12fac05c9dfe4ba4cb
TOME Olivier <olivier.tome@sinequa.com>
2024-10-04 16:10:36 +0200

    (search input): handle keyboard behavior
    ## Update @sinequa/atomic-angular to version 0.0.81
    This version includes the `debounceSignal()` function used by the search-input component
    
    ## search-input
    - removed rxjs debounce code to use a signal version.
    - manage the autocomplete popover open with the keyboard
    - added a computed signal `popoverElement` to avoid the use of the `nativeElement` property everywhere in the code

--- src/core/components/search-input/search-input.component.ts
+++ src/core/components/search-input/search-input.component.ts
@@ -1,13 +1,11 @@
 import { NgClass } from '@angular/common';
-import { Component, ElementRef, OnDestroy, booleanAttribute, computed, effect, inject, input, output, signal, viewChild } from '@angular/core';
-import { toObservable } from '@angular/core/rxjs-interop';
+import { Component, ElementRef, Signal, booleanAttribute, computed, effect, inject, input, output, signal, viewChild } from '@angular/core';
 import { FormsModule } from '@angular/forms';
 import { ActivatedRoute } from '@angular/router';
 import { HashMap, Translation, TranslocoPipe, TranslocoService, provideTranslocoScope } from '@jsverse/transloco';
 import { getState } from '@ngrx/signals';
-import { AppStore, AutocompleteService, CJson, DrawerStackService, QueryParamsStore } from '@sinequa/atomic-angular';
+import { AppStore, AutocompleteService, CJson, debouncedSignal, DrawerStackService, QueryParamsStore } from '@sinequa/atomic-angular';
 import { toast } from 'ngx-sonner';
-import { Subscription, debounceTime, filter } from 'rxjs';
 
 const DEBOUNCE_DELAY = 300;
 
@@ -24,13 +22,14 @@ const loader = ['en', 'fr'].reduce((acc, lang) => {
   styleUrl: './search-input.component.scss',
   providers: [provideTranslocoScope({ scope: 'searchInput', loader })]
 })
-export class SearchInputComponent implements OnDestroy {
+export class SearchInputComponent {
   readonly debounced = output<string>();
   readonly validated = output<string>();
   readonly saved = output<void>();
   readonly clicked = output<void>();
 
   private readonly autocompletePopover = viewChild<ElementRef>('autocompletePopover');
+  private readonly popoverElement: Signal<HTMLDivElement> = computed(() => this.autocompletePopover()?.nativeElement);
 
   protected readonly autocompleteService = inject(AutocompleteService);
   private readonly drawerStack = inject(DrawerStackService);
@@ -39,12 +38,14 @@ export class SearchInputComponent implements OnDestroy {
   private readonly route = inject(ActivatedRoute);
   private readonly translocoService = inject(TranslocoService);
 
-  protected readonly autocompletePopoverOpened = computed(() => this.autocompletePopover()?.nativeElement?.isPopoverOpen);
   public readonly showSave = input(false, { transform: booleanAttribute });
 
-  protected readonly allowChatDrawer = signal<boolean>(false);
   public readonly input = signal<string>('');
-  protected saveAnimation = signal<boolean>(false);
+
+  protected readonly allowChatDrawer = signal<boolean>(false);
+  protected readonly saveAnimation = signal<boolean>(false);
+
+  protected readonly debounceInputValue = debouncedSignal(this.input, DEBOUNCE_DELAY);
 
   protected allowEmptySearch = computed(() => {
     const { queryName } = this.route.snapshot.data;
@@ -52,9 +53,14 @@ export class SearchInputComponent implements OnDestroy {
   });
 
 
-  private readonly _subscription = new Subscription();
-
+  // el is the ElementRef of the component, it is injected by Angular and used by the AutoComplete component
   constructor(public readonly el: ElementRef) {
+    effect(() => {
+      const value = this.debounceInputValue();
+      this.debounced.emit(value);
+    });
+
+
     effect(() => {
       const { data } = getState(this.appStore);
       const { features = { allowChatDrawer: false } } = data as CJson;
@@ -62,32 +68,20 @@ export class SearchInputComponent implements OnDestroy {
       this.allowChatDrawer.set(features.allowChatDrawer);
     }, { allowSignalWrites: true })
 
+    // first time the component is created, we set the input value from the query params
     effect(() => {
       const { text } = getState(this.queryParamsStore);
       this.setInput(text);
     }, { allowSignalWrites: true })
 
-
-    this._subscription.add(
-      toObservable(this.input)
-        .pipe(
-          filter(text => text !== undefined),
-          debounceTime(DEBOUNCE_DELAY)
-        )
-        .subscribe(text => this.debounced.emit(text))
-    )
-  }
-
-  ngOnDestroy(): void {
-    this._subscription.unsubscribe();
   }
 
   public closeAutocompletePopover(): void {
-    this.autocompletePopover()?.nativeElement?.hidePopover();
+    this.popoverElement().hidePopover();
   }
 
   public inputClicked(): void {
-    this.autocompletePopover()?.nativeElement?.showPopover();
+    this.popoverElement().showPopover();
     this.clicked.emit();
   }
 
@@ -109,6 +103,7 @@ export class SearchInputComponent implements OnDestroy {
       return;
     }
 
+    this.closeAutocompletePopover();
     this.validated.emit(this.input());
   }
 
@@ -125,5 +120,21 @@ export class SearchInputComponent implements OnDestroy {
     setTimeout(() => this.saveAnimation.set(false), 1000);
 
     this.saved.emit();
+  }
+
+  /**
+   * Handles the keydown event on the search input.
+   *
+   * @param e - The keyboard event triggered by the user.
+   *
+   * If the 'Enter' key is pressed, the current text is emitted.
+   * If the input is not empty and a different key is pressed, the popover is shown (if previously hidden).
+   */
+  protected onKeyDown(e: KeyboardEvent): void {
+    if (e.key === 'Enter') {
+      this.emitText();
+    } else if(this.input() !== ''){
+      this.popoverElement().showPopover();
+    }
   }
 }
