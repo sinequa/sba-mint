9c1e7d5d7ad8b96416167ffc75569094f5a21d2b
Juillet <loic.juillet@sinequa.com>
2024-11-19 13:47:37 +0100

    Update src/core/components/filters/filters.models.ts

--- src/core/components/filters/filters.models.ts
+++ src/core/components/filters/filters.models.ts
@@ -13,5 +13,3 @@ export type CFilterEx = CFilter & {
 };
 
 export const FILTERS_BREAKPOINT = new InjectionToken<number>("FILTERS_BREAKPOINT", { factory: () => 5 });
-export const AUTHORED_BY_ME_DOCUMENT_FILTER_COLUMN = new InjectionToken("Authored by me document filter column", {factory: () => "employeeAuthorID"});
-export const AUTHORED_BY_ME_PERSON_FILTER_COLUMN = new InjectionToken("Authored by me person filter column", {factory: () => "employeeAuthorID"});
