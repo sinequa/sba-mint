5d996c26132e2faeca36ae74a041b495a64378c7
TOME Olivier <olivier.tome@sinequa.com>
2024-11-22 16:41:39 +0100

    ♻️ (refactor) search
    - move document-type-registry to app directory and update imports
    - add `search.abstract` class to simplify component's search creation.
    - update `search-all.component` to inherits from `search.abstract` class

--- src/app/pages/search/all/search-all.component.ts
+++ src/app/pages/search/all/search-all.component.ts
@@ -1,18 +1,16 @@
 import { NgComponentOutlet } from '@angular/common';
-import { Component, HostBinding, OnDestroy, Type, computed, effect, inject, input, signal } from '@angular/core';
-import { getState } from '@ngrx/signals';
-import { injectInfiniteQuery } from '@tanstack/angular-query-experimental';
-import { Subscription, catchError, lastValueFrom, map, of } from 'rxjs';
+import { Component, HostBinding, Type } from '@angular/core';
 
-import { Aggregation, Article, Query, Result } from '@sinequa/atomic';
-import { AggregationsStore, DrawerStackService, InfinityScrollDirective, PrincipalStore, QueryParamsStore, SearchService, SelectionService } from '@sinequa/atomic-angular';
+import { Result } from '@sinequa/atomic';
+import { InfinityScrollDirective } from '@sinequa/atomic-angular';
 
+import { getComponentsForDocumentType } from '@/app/registry/document-type-registry';
 import { ArticleDefaultSkeletonComponent } from '@/core/components/article/default-skeleton/article-default-skeleton.component';
 import { FiltersListComponent } from "@/core/components/filters/filters-list.component";
 import { SponsoredResultsComponent } from "@/core/components/sponsored-results/sponsored-results.component";
 import { DidYouMeanComponent } from '@/core/features/did-you-mean/did-you-mean.component';
 import { SortSelectorComponent, SortingChoice } from '@/core/features/sort-selector/sort-selector.component';
-import { getComponentsForDocumentType } from '@/core/registry/document-type-registry';
+import { SearchBase } from '../search.abstract';
 
 type R = Result & { nextPage?: number, previousPage?: number };
 
@@ -34,128 +32,12 @@ type R = Result & { nextPage?: number, previousPage?: number };
     class: 'layout-search overflow-auto h-full'
   }
 })
-export class SearchAllComponent implements OnDestroy {
+export class SearchAllComponent extends SearchBase<R> {
   @HostBinding('attr.drawer-opened')
-  public drawerOpened: boolean = false;
 
-  readonly result = signal<Result | undefined>(undefined);
-  protected readonly articles = signal(undefined as Article[] | undefined);
-  protected readonly queryText = signal<string>('');
-
-  private readonly searchService = inject(SearchService);
-  private readonly drawerStack = inject(DrawerStackService);
-  private readonly aggregationsStore = inject(AggregationsStore);
-  private readonly queryParamsStore = inject(QueryParamsStore);
-  private readonly selectionService = inject(SelectionService);
-  private readonly principalStore = inject(PrincipalStore);
-
-  protected aggregations: Aggregation[];
-
-  private readonly sub = new Subscription();
-
-  // track the query params store changes
-  keys = computed(() => {
-    const state = getState(this.queryParamsStore);
-    return { text: state.text, filters: state.filters, sort: state.sort }
-  });
-
-  // get the id from the query params store to open the drawer with the preview of the article
-  id = computed(() => {
-    const state = getState(this.queryParamsStore);
-    return state.id;
-  });
-
-  // the query must be retriggered when the user override is active
-  userOverrideActive = computed(() => {
-    const state = getState(this.principalStore);
-    return state.userOverrideActive;
-  });
-
-
-  // input url bindings
-  q = input<string>(); // text
-  t = input<string>(); // tab
-  s = input<string>(); // sort
-  f = input<string>(); // filters
-  queryName = input<string>(); // query param
-
-
-  // tanstack query
-  query = injectInfiniteQuery<R>(() => ({
-    queryKey: [`search-${this.t()}`, this.keys(), this.userOverrideActive()],
-    queryFn: ({ pageParam }) => {
-      const q = this.queryParamsStore.getQuery();
-      const query = { ...q, page: pageParam, tab: this.t(), name: this.queryName() } as Query;
-
-      return lastValueFrom(this.searchService.getResult(query).pipe(
-        map(result => {
-          // If the id is set, open the drawer with the preview of the article
-          const id = this.id();
-          if (id) {
-            result.records?.forEach(article => {
-              if (article.id === id) {
-                this.selectionService.setCurrentArticle(article);
-                this.drawerStack.open();
-              }
-            });
-          }
-          return result;
-        })
-      ));
-    },
-    initialPageParam: 1,
-    getPreviousPageParam: (firstPage) => (firstPage.previousPage ?? undefined),
-    getNextPageParam: (lastPage) => (lastPage.nextPage ?? undefined),
-  }));
-
-  constructor() {
-    // Update the query params store with the filters from the query params
-    // This allows Browser back/forward to work correctly
-    effect(() => {
-      const filters = this.f() ? JSON.parse(this.f() ?? '') : []; // Parse the filters from the query params
-      this.queryParamsStore.patch({ text: this.q(), tab: this.t(), sort: this.s(), filters, name: this.queryName() });
-    }, { allowSignalWrites: true });
-
-    this.sub.add(
-      this.drawerStack.isOpened.subscribe(state => this.drawerOpened = state)
-    );
-
-    // Trigger skeleton on search whether from input or from filters
-    effect(() => {
-      getState(this.queryParamsStore);
-
-      // Reset the articles when the query params store changes
-      this.articles.set(undefined);
-
-      // update URL Search Params when the query params store changes
-      this.searchService.search([]);
-
-    }, { allowSignalWrites: true });
-
-    // Make Result object available to children and update aggregations store
-    effect(() => {
-      this.query.isSuccess();
-
-      const result = this.query.data()?.pages[0];
-
-      if (!result) return;
-
-      this.result.set(result);
-
-      // Update the aggregations store with the new aggregations
-      this.aggregationsStore.update(result.aggregations);
-    }, { allowSignalWrites: true });
-  }
-
-  ngOnDestroy(): void {
-    this.sub.unsubscribe();
-    this.aggregationsStore.clear();
-  }
 
   onSort(sort: SortingChoice): void {
     this.queryParamsStore.patch({ sort: sort.name });
-
-    this.articles.set(undefined);
     this.searchService.search([], {
       audit: {
         type: "Search_Sort",
@@ -167,9 +49,6 @@ export class SearchAllComponent implements OnDestroy {
     });
   }
 
-  nextPage() {
-    this.query.fetchNextPage();
-  }
 
   getArticleType(docType: string): Type<unknown> {
     return getComponentsForDocumentType(docType).articleComponent;
