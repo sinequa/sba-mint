f55f011518bc18ac37e3fdc06c3d8913590ed2c0
Guillaume Leibfried <guillaume.leibfried@sinequa.com>
2025-01-03 13:56:57 +0100

    refactor

--- src/core/components/article/missing-terms/missing-terms.component.ts
+++ src/core/components/article/missing-terms/missing-terms.component.ts
@@ -1,5 +1,5 @@
 import { Component, computed, inject, input } from '@angular/core';
-import { Router } from '@angular/router';
+import { Router, RouterLink } from '@angular/router';
 import { HashMap, provideTranslocoScope, Translation, TranslocoPipe } from '@jsverse/transloco';
 import { Article } from '@sinequa/atomic';
 import { QueryParamsStore, SearchService } from '@sinequa/atomic-angular';
@@ -10,13 +10,13 @@ const loader = ['en', 'fr'].reduce((acc, lang) => {
 }, {} as HashMap<() => Promise<Translation>>);
 
 @Component({
-  selector: 'app-missing-terms',
+  selector: 'missing-terms',
   standalone: true,
-  imports: [TranslocoPipe],
+  imports: [TranslocoPipe, RouterLink],
   templateUrl: './missing-terms.component.html',
   providers: [provideTranslocoScope({ scope: 'article', loader })],
   styles: `
-.sq-missing-term {
+.term {
     text-decoration-line: line-through;
 }
   `
@@ -29,14 +29,13 @@ export class MissingTermsComponent {
   router = inject(Router);
 
   public missingTerms = computed(() => {
-    return this.article().termspresence
-      ?.filter(tp => tp.presence === 'missing')
-      .map(tp => tp.term) || [];
-  });
-
-  mustInclude(term: string) {
     const query = this.queryParamsStore.getQuery();
-    const text = (query.text || '').replace(new RegExp(`\\b${term}\\b`, 'gi'), "");
-    this.router.navigate(['/search'], { queryParams: { q: `${text}+[${term}]` } });
-  }
+    return this.article().termspresence?.filter(tp => tp.presence === 'missing').map(tp => {
+      const text = (query.text || '').replace(new RegExp(`\\b${tp.term}\\b`, 'gi'), "");
+      return ({
+        value: tp.term,
+        queryParams: { q: `${text}+[${tp.term}]` }
+      })
+    });
+  })
 }
