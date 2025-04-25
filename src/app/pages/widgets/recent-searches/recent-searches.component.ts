42922d8dfd88135e81f8f13c487dd9837eba079f
Guillaume Leibfried <guillaume.leibfried@sinequa.com>
2025-04-25 11:02:58 +0200

    change collections page icon + small layout update

--- src/app/pages/widgets/recent-searches/recent-searches.component.ts
+++ src/app/pages/widgets/recent-searches/recent-searches.component.ts
@@ -21,7 +21,7 @@ import { AppSidebarComponent } from '../../../components/sidebar/sidebar.compone
 
     <div class="layout-search overflow-auto">
       <div class="col-span-2 col-start-2">
-        <h1 class="mt-6 mb-4 flex gap-2 text-2xl font-semibold">
+        <h1 class="mt-6 mb-4 flex items-center gap-2 text-2xl font-semibold">
           <i class="fa-fw far fa-clock-rotate-left" aria-hidden></i>
           {{ 'history' | transloco }}
         </h1>
