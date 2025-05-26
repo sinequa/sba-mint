6bffe6140a43df6d55bb03c17fd105cf0a588b07
Olivier.T <otome@chapsvision.com>
2025-05-26 16:30:06 +0200

    chore: refactor app component (#310)
    route.ts
    - removed InitializationGuard() useless with the new
    bootstrapApp() function
    
    app.component.ts
    - removed old useless code. The component now setup the logo images and set the language
    
    app.config.ts
    - use of bootstrapApp() function instead of signIn() function. This new function initialize the app after been authenticated
    
    update @sinequa/atomic-angular to version 0.0.241-alpha.1
    
    Co-authored-by: TOME Olivier <olivier.tome@sinequa.com>

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,25 +1,14 @@
-import { CommonModule } from '@angular/common';
-import { Component, computed, DestroyRef, effect, inject } from '@angular/core';
+import { Component, effect, inject } from '@angular/core';
 import { Title } from '@angular/platform-browser';
-import { Router, RouterOutlet } from '@angular/router';
 import { TranslocoService } from '@jsverse/transloco';
-import { getState } from '@ngrx/signals';
 import { NgxSonnerToaster } from 'ngx-sonner';
 
-import { CCApp, getHelpIndexUrl, globalConfig, isAuthenticated } from '@sinequa/atomic';
-import {
-  ApplicationService,
-  ApplicationStore,
-  AppStore,
-  BackdropComponent,
-  DrawerStackComponent,
-  PrincipalStore,
-  UserSettingsStore
-} from '@sinequa/atomic-angular';
+import { RouterOutlet } from '@angular/router';
+import { ApplicationStore, AppStore, BackdropComponent, DrawerStackComponent, UserSettingsStore } from '@sinequa/atomic-angular';
 
 @Component({
   selector: 'app-root',
-  imports: [CommonModule, RouterOutlet, NgxSonnerToaster, BackdropComponent, DrawerStackComponent],
+  imports: [RouterOutlet, NgxSonnerToaster, BackdropComponent, DrawerStackComponent],
   templateUrl: './app.component.html',
   styles: [
     `
@@ -30,59 +19,15 @@ import {
   ]
 })
 export class AppComponent {
-  private readonly applicationService = inject(ApplicationService);
   private readonly userSettingsStore = inject(UserSettingsStore);
   private readonly appStore = inject(AppStore);
   private readonly transloco = inject(TranslocoService);
 
-  private readonly router = inject(Router);
   private readonly title = inject(Title);
 
   private readonly applicationStore = inject(ApplicationStore);
-  private readonly principalStore = inject(PrincipalStore);
 
-  protected readonly allowAI = computed(() => this.appStore.isAssistantAllowed(this.instanceId()));
-  readonly instanceId = computed(() => {
-    const { name } = getState(this.appStore) as CCApp;
-    return `${name}-standalone-assistant`;
-  });
-
-  protected readonly authenticated = computed(() => isAuthenticated());
-  readonly isAdmin = computed(() => this.principalStore.principal().isAdministrator || this.principalStore.principal().isDelegatedAdmin);
-
-  constructor(private destroyRef: DestroyRef) {
-    const controller = new AbortController();
-    addEventListener(
-      'authenticated',
-      (event: Event) => {
-        const customEvent = event as CustomEvent;
-
-        const { authenticated } = customEvent.detail;
-        if (authenticated) {
-          console.log('User authenticated (from event), initializing application...');
-          this.initApplication();
-        }
-      },
-      { signal: controller.signal }
-    );
-
-    destroyRef.onDestroy(() => {
-      controller.abort();
-    });
-
-    const { useSSO } = globalConfig;
-    if (useSSO) {
-      this.initApplication();
-    } else {
-      if (isAuthenticated()) {
-        this.initApplication();
-
-        if (this.router.url === '/error') {
-          this.router.navigate(['/']);
-        }
-      }
-    }
-
+  constructor() {
     effect(() => {
       const general = this.appStore.general();
 
@@ -98,39 +43,14 @@ export class AppComponent {
         }
       }
     });
-  }
 
-  initApplication() {
-    this.applicationService
-      .initAndCreateRoutes()
-      .then(() => {
-        this.setupApplicationLanguage();
-        this.applicationStore.updateReadyState(true);
-      })
-      .catch(err => {
-        console.error('Error initializing application', err);
-        this.applicationStore.updateReadyState(false);
-      });
+    this.setupApplicationLanguage();
+    this.applicationStore.updateReadyState(true);
   }
 
   private setupApplicationLanguage() {
     if (this.userSettingsStore.language?.() === undefined) this.userSettingsStore.updateLanguage('en');
 
     this.transloco.setActiveLang(this.userSettingsStore.language?.() ?? 'en');
-  }
-
-  openHelp() {
-    const url = getHelpIndexUrl(this.transloco.getActiveLang(), {
-      folder: 'mint-search',
-      path: '/r/_sinequa/webpackages/help',
-      indexFile: 'olh-index.html',
-      useLocale: true,
-      useLocaleAsPrefix: true
-    });
-    window.open(url, '_blank', 'noopener');
-  }
-
-  openAdmin() {
-    window.open(`${window.location.origin}/admin`, '_blank', 'noopener');
   }
 }
