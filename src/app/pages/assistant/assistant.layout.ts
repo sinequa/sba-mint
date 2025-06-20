c78d5c168d11291c6c0863afc2d2672cc66e813d
Guillaume Leibfried <guillaume.leibfried@sinequa.com>
2025-06-20 16:08:31 +0200

    add notification event listening for the assistant

--- src/app/pages/assistant/assistant.layout.ts
+++ src/app/pages/assistant/assistant.layout.ts
@@ -4,7 +4,7 @@ import { provideTranslocoScope, TranslocoPipe } from '@jsverse/transloco';
 import { HubConnection } from '@microsoft/signalr';
 import { getState } from '@ngrx/signals';
 
-import { SavedChatsComponent } from '@sinequa/assistant/chat';
+import { NotificationType, SavedChatsComponent } from '@sinequa/assistant/chat';
 import { CCApp, fetchQuery } from '@sinequa/atomic';
 import { AggregationComponent, AggregationsStore, AppStore, DrawerStackService, SelectionStore } from '@sinequa/atomic-angular';
 import { ButtonComponent, cn, PageHeaderComponent } from '@sinequa/ui';
@@ -14,6 +14,7 @@ import { AssistantComponent } from '../../components/assistant/assistant';
 import { NavbarComponent } from '../../components/navbar/navbar.component';
 import { AssistantUploadComponent } from './document-upload/assistant-upload.component';
 import { AppSidebarComponent } from '../../components/sidebar/sidebar.component';
+import { toast } from 'ngx-sonner';
 
 @Component({
   selector: 'assistant-layout, AssistantLayout',
@@ -145,6 +146,8 @@ export class AssistantLayoutComponent {
 
     // this is needed to populate the aggregation with the sources as no query is sent to the server
     this.getFirstPageQuery();
+
+    this.watchAssistantNotification();
   }
 
   async getFirstPageQuery() {
@@ -165,5 +168,17 @@ export class AssistantLayoutComponent {
     if (ready) {
       this.isAssistantReady.set(true);
     }
+  }
+
+  watchAssistantNotification(): void {
+    addEventListener('notification', (event: Event) => {
+      const customEvent = event as CustomEvent<{
+        type: NotificationType;
+        title?: string;
+        message: string;
+      }>;
+      const { type, message } = customEvent.detail;
+      toast[type](message);
+    });
   }
 }
