40ec541373ec38180cf6b4e3d04cb53a4c36fc9b
Olivier.T <otome@chapsvision.com>
2025-08-04 07:54:02 +0200

    🩹 ES-27741 #done fix: "AskAI" button (#416)
    The query text is automatically deleted when the Assistant layout component was mounted. This is not anymore the case.
    
    - reinstatement of the text ‘no suggestion’, deleted in a previous redone by mistake

--- src/app/pages/assistant/assistant.layout.ts
+++ src/app/pages/assistant/assistant.layout.ts
@@ -130,8 +130,6 @@ export class AssistantLayoutComponent {
   q = input<string>();
 
   constructor() {
-    this.queryParamsStore.patch({ text: undefined, tab: undefined, basket: undefined });
-
     effect(() => {
       // force the change detection when the AggregationStore is updated.
       // This is needed because we use the ChatComponent which is not a signal component (i.e Angular v14)
