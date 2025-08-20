97eb3e521fe74788aea9a4a6788666b1b0cb1270
olivier.T <otome@chapsvision.com>
2025-08-20 17:30:12 +0200

    ES-27974 #done fix: update menu display labels for recent and saved searches
    The i18n key has changed to 'searches.recent' and 'searches.saved' insteod of 'recentSearches" and "savedSearches" because the 2 components share now the same inner components

--- src/app/components/navbar/navbar.component.ts
+++ src/app/components/navbar/navbar.component.ts
@@ -75,10 +75,10 @@ export class NavbarComponent {
   readonly searchText = signal<string>('');
 
   protected readonly menus = signal<NavbarMenu[]>([
-    { display: 'recentSearches.label', iconClass: 'far fa-clock-rotate-left', routerLink: '/widgets/recent-searches', component: RecentSearchesComponent },
+    { display: 'searches.recent.label', iconClass: 'far fa-clock-rotate-left', routerLink: '/widgets/recent-searches', component: RecentSearchesComponent },
     { display: 'bookmarks.label', iconClass: 'far fa-bookmark', routerLink: '/widgets/bookmarks', component: BookmarksComponent },
     { display: 'collections.label', iconClass: 'far fa-inbox', routerLink: '/widgets/collections', component: CollectionsComponent },
-    { display: 'savedSearches.label', iconClass: 'far fa-star', routerLink: '/widgets/saved-searches', component: SavedSearchesComponent },
+    { display: 'searches.saved.label', iconClass: 'far fa-star', routerLink: '/widgets/saved-searches', component: SavedSearchesComponent },
     { display: 'alerts.label', iconClass: 'far fa-bell', component: AlertsComponent }
   ]);
 
