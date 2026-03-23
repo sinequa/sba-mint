2a5dd0fecc40c260ae0f19dda4433c5712b2ab24
AzureAD\MahdiJALOULI <mjalouli.ext@chapsvision.com>
2026-03-23 11:21:58 +0100

    remove test code

--- src/app2/pages/search/search-all.ts
+++ src/app2/pages/search/search-all.ts
@@ -239,8 +239,7 @@ export class SearchAllComponent {
     }
     return `search-results-assistant`;
   });
-  // readonly allowAI = computed(() => !this.b() && this.appStore.isAssistantAllowed(this.instanceId()));
-  readonly allowAI = signal(true);
+  readonly allowAI = computed(() => !this.b() && this.appStore.isAssistantAllowed(this.instanceId()));
   readonly enabledUserInput = computed(() => this.appStore.assistants()[this.instanceId()]?.["modeSettings"]?.["enabledUserInput"] === true);
   // assistantQuery: Query = { name: 'assistant' };
 
