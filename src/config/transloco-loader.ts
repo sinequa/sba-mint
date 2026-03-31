544e903549cf4f2d26e484d11bff4f97647c28b8
olivier.T <otome@chapsvision.com>
2026-03-31 17:31:24 +0200

    ES-31242 #done fix(i18n): handle translation loading errors in TranslocoHttpLoader
    Add catchError to the translation HTTP request to return an empty
    object on failure, preventing infinite resource fetching loops when
    a translation file cannot be loaded.

--- src/config/transloco-loader.ts
+++ src/config/transloco-loader.ts
@@ -1,14 +1,20 @@
-import { HttpClient } from '@angular/common/http';
-import { inject, Injectable } from '@angular/core';
-import { Translation, TranslocoLoader } from '@jsverse/transloco';
+import { HttpClient } from "@angular/common/http";
+import { Injectable, inject } from "@angular/core";
+import { Translation, TranslocoLoader } from "@jsverse/transloco";
+import { catchError, of } from "rxjs";
 
 @Injectable({
-  providedIn: 'root'
+  providedIn: "root"
 })
 export class TranslocoHttpLoader implements TranslocoLoader {
   private http = inject(HttpClient);
 
   getTranslation(lang: string) {
-    return this.http.get<Translation>(`assets/i18n/${lang}.json`);
+    return this.http.get<Translation>(`assets/i18n/${lang}.json`).pipe(
+      catchError(err => {
+        // to avoid infinite resources fetching
+        return of({});
+      })
+    );
   }
 }
