e26b4896fba1d88ade6aa6b4be9d24b7decd161e
AzureAD\MahdiJALOULI <mjalouli.ext@chapsvision.com>
2026-04-13 17:47:03 +0100

    Add translations for sidebar and search component placeholders

--- src/app2/components/sidebar.ts
+++ src/app2/components/sidebar.ts
@@ -103,18 +103,30 @@ import { filter } from "rxjs";
 
       <sidebar-footer class="px-3 py-6">
         <sidebar-menu>
-          @if (isAdminOrDelegatedAdmin()) {
-            <sidebar-menu-item [attr.aria-label]="'Administration'" (click)="openAdmin()">
+           @if (isAdminOrDelegatedAdmin()) {
+            @let administration = ('administration' | transloco);
+            <sidebar-menu-item
+              [attr.aria-label]="administration"
+              (click)="openAdmin()"
+              [tooltip]="administration"
+              tooltip-position="right"
+            >
               <sidebar-menu-button class="text-lg">
                 <gear-icon aria-hidden="true" />
-                <span class="text-sm" sr-only>{{ 'administration' | transloco }}</span>
+                <span class="text-sm" sr-only>{{ administration }}</span>
               </sidebar-menu-button>
             </sidebar-menu-item>
           }
-          <sidebar-menu-item [attr.aria-label]="'Help'" (click)="openHelp()">
+          @let help = ('help' | transloco);
+          <sidebar-menu-item
+            [attr.aria-label]="help"
+            (click)="openHelp()"
+            [tooltip]="help"
+            tooltip-position="right"
+          >
             <sidebar-menu-button class="text-lg">
               <question-circle-icon aria-hidden="true" />
-              <span class="text-sm" sr-only>{{ 'help' | transloco }}</span>
+              <span class="text-sm" sr-only>{{ help }}</span>
             </sidebar-menu-button>
           </sidebar-menu-item>
 
